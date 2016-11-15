69a3d64ae17ff1023c867450527f98208ed09685
JianDong <W-JD@users.noreply.github.com>
2016-11-15 13:48:04 -0500

    Update code.js

--- code.js
+++ code.js
@@ -3,29 +3,29 @@ W-JD https://github.com/W-JD
 **/
 
 function htmlEncode(value){
- -  //create a in-memory div, set it's inner text(which jQuery automatically encodes)
- -  //then grab the encoded contents back out.  The div never exists on the page.
- -  return $('<div/>').text(value).html();
- -}
- -function htmlDecode(value){
- -  return $('<div/>').html(value).text();
- -}
- -function makeHttpObject() {
- -  try {return new XMLHttpRequest();}
- -  catch (error) {}
- -  try {return new ActiveXObject("Msxml2.XMLHTTP");}
- -  catch (error) {}
- -  try {return new ActiveXObject("Microsoft.XMLHTTP");}
- -  catch (error) {}
- -
- -  throw new Error("Could not create HTTP request object.");
- -}
- -var request = makeHttpObject();
- -request.open("GET", url, true);
- -request.send(null);
- -request.onreadystatechange = function() {
- -  if (request.readyState == 4)
- -var htmlcode = htmlEncode(request.responseText)
- -document.getElementById("code").innerHTML=htmlcode;
- -hljs.initHighlightingOnLoad();
- -};
+   //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+   //then grab the encoded contents back out.  The div never exists on the page.
+   return $('<div/>').text(value).html();
+ }
+ function htmlDecode(value){
+   return $('<div/>').html(value).text();
+ }
+ function makeHttpObject() {
+   try {return new XMLHttpRequest();}
+   catch (error) {}
+   try {return new ActiveXObject("Msxml2.XMLHTTP");}
+   catch (error) {}
+   try {return new ActiveXObject("Microsoft.XMLHTTP");}
+   catch (error) {}
+ 
+   throw new Error("Could not create HTTP request object.");
+ }
+ var request = makeHttpObject();
+ request.open("GET", url, true);
+ request.send(null);
+ request.onreadystatechange = function() {
+   if (request.readyState == 4)
+ var htmlcode = htmlEncode(request.responseText)
+ document.getElementById("code").innerHTML=htmlcode;
+ hljs.initHighlightingOnLoad();
+ };

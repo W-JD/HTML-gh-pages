4debb2a60483487f7e6f4ffbaa5eea68af4e5b05
JianDong <W-JD@users.noreply.github.com>
2016-11-15 15:02:13 -0500

    Update url.js

--- url.js
+++ url.js
@@ -1,10 +1,10 @@
-function getParameterByName(name, url) {
-    if (!url) {
-      url = window.location.href;
+function getParameterByName(name, urls) {
+    if (!urls) {
+      urls = window.location.href;
     }
     name = name.replace(/[\[\]]/g, "\\$&");
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
+        results = regex.exec(urls);
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));

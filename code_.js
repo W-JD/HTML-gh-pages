187e1acf0d59dc0834f2dfbc5a4cc1defcc48351
JianDong <W-JD@users.noreply.github.com>
2016-11-15 16:26:08 -0500

    Create code_.js

---
+++ code_.js
@@ -0,0 +1,11 @@
+var request = makeHttpObject();
+request.open("GET", url, true);
+request.send(null);
+request.onreadystatechange = function() {
+var htmlcode = htmlEncode(request.responseText)
+ document.getElementById("code").innerHTML=htmlcode;
+
+hljs.configure({
+   languages: "HTML"     
+})
+hljs.initHighlighting();

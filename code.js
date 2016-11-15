146ac1db228b76f2ae3ff5f432f4ce1a1a92e285
JianDong <W-JD@users.noreply.github.com>
2016-11-15 12:34:29 -0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,27 @@
+function htmlEncode(value){
+  //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+  //then grab the encoded contents back out.  The div never exists on the page.
+  return $('<div/>').text(value).html();
+}
+function htmlDecode(value){
+  return $('<div/>').html(value).text();
+}
+function makeHttpObject() {
+  try {return new XMLHttpRequest();}
+  catch (error) {}
+  try {return new ActiveXObject("Msxml2.XMLHTTP");}
+  catch (error) {}
+  try {return new ActiveXObject("Microsoft.XMLHTTP");}
+  catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
+var request = makeHttpObject();
+request.open("GET", url, true);
+request.send(null);
+request.onreadystatechange = function() {
+  if (request.readyState == 4)
+var htmlcode = htmlEncode(request.responseText)
+document.getElementById("code").innerHTML=htmlcode;
+hljs.initHighlightingOnLoad();
+};

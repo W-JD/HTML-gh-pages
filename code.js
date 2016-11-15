dcafd0ce0ebd5779baa74fd9fef56f002ece4941
JianDong <W-JD@users.noreply.github.com>
2016-11-15 16:06:35 -0500

    Update code.js

--- code.js
+++ code.js
@@ -26,14 +26,9 @@ request.onreadystatechange = function() {
   if (request.readyState == 4)
     var htmlcode = htmlEncode(request.responseText)
     document.getElementById("code").innerHTML=htmlcode;
-hljs.configure({
+    hljs.configure({
    languages: "HTML"     
-})
-    hljs.initHighlighting();
-  else
-     document.getElementById("code").innerHTML=htmlcode;
-hljs.configure({
-   languages: "HTML"     
-})
-    hljs.initHighlighting();
-};
+      })
+      hljs.initHighlighting();
+
+  };

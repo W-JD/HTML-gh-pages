85f0695addcd205674a7b41f0195e8f81bbc3252
JianDong <W-JD@users.noreply.github.com>
2016-11-16 09:09:08 -0500

    Update code.js

--- code.js
+++ code.js
@@ -19,3 +19,11 @@ function makeHttpObject() {
   catch (error) {}
   throw new Error("Could not create HTTP request object.");
 }
+
+var request = makeHttpObject();
+request.open("GET", url, true);
+request.send(null);
+request.onreadystatechange = function() {
+var htmlcode = htmlEncode(request.responseText);
+ document.getElementById("code").innerHTML=htmlcode;
+};

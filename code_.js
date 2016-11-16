db5c99d655aa055bb784d62aedc33cf0a6b6b60d
JianDong <W-JD@users.noreply.github.com>
2016-11-16 09:16:10 -0500

    Delete code_.js

--- code_.js
+++
@@ -1,7 +0,0 @@
-var request = makeHttpObject();
-request.open("GET", url, true);
-request.send(null);
-request.onreadystatechange = function() {
-var htmlcode = htmlEncode(request.responseText);
- document.getElementById("code").innerHTML=htmlcode;
-};
